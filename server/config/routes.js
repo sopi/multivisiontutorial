a554ef3322533e3c467395dd1f6bb31a025d2329
Piotr Sobczak <piotr.so@gmail.com>
2014-09-01 18:28:01 +0200

    Working login

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,21 +5,7 @@ module.exports = function(app){
         res.render('../../public/app/' + req.params[0]);
     });
 
-//    app.post('/login', function(req, res, next){
-//        var  auth = passport.authenticate('local', function (err, user) {
-//            if(err) { console.log('route ' + err); return next(err); }
-//            if(!user) { res.send({success:false}); }
-//
-//            req.logIn(user, function (err) {
-//                if(err) { return next(err); }
-//                res.send({success: true, user: user});
-//            });
-//        });
-//        auth(req, res, next);
-//    });
-
     app.post('/login', function(req, res, next) {
-
         passport.authenticate('local', function(err, user) {
             console.log('check route: user: ' + user);
             if (err) { return next(err) }
@@ -32,7 +18,8 @@ module.exports = function(app){
             });
         })(req, res, next);
     });
+
     app.get('*', function  (req, res) {
         res.render('index');
     });
-}
+};
